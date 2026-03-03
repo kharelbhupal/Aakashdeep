2a16c2674967069f2fd6f59893e4e2e2a1686793
Bhupal Kharel <kharelbhupal@gmail.com>
2026-03-03 20:25:26 +0545

    Favicon added and social link updated everywhere

--- services.js
+++ services.js
@@ -10,3 +10,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll(".animate-on-scroll");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  });
+
+  elements.forEach((el) => observer.observe(el));
+});
